c03a074e5efa63bfc7da3624b9ad9d03e5a21f6c
escnqh <844071773@qq.com>
2017-09-21 17:42:19 +0800

    push

---
+++ app/src/main/java/com/ntanougat/test4ndk/MyNDKTest.java
@@ -0,0 +1,13 @@
+package com.ntanougat.test4ndk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyNDKTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_ndktest);
+    }
+}
